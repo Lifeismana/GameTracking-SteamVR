d6e245996718197a7fb89a0ce5342a83d2988510
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2026-03-13 13:10:29 +0100

    cache tools

--- update.sh
+++ update.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 cd "${0%/*}"
 . ../common.sh
 
+
+echo "Downloading SteamVR..."
+
+$STEAM_FILE_DOWNLOADER_PATH --username "$STEAM_USERNAME" --password "$STEAM_PASSWORD" --appid 250820 --output . --branch $STEAM_BRANCH --save-manifest
+
 echo "Processing SteamVR..."
 
 set +e
@@ -22,6 +27,6 @@ rm --recursive resources/webinterface/dashboard_prettier/ || true
 rsync --archive --include="*/" --include="*.js" --include="*.css" --exclude="*" --prune-empty-dirs resources/webinterface/dashboard/ resources/webinterface/dashboard_prettier/
 npm run prettier -- --write resources/webinterface/dashboard_prettier/ || true
 
-CreateCommit "$(grep -o '[0-9\.]*' bin/version.txt)" "$1"
+CreateCommit "$(grep -o '[0-9\.]*' bin/version.txt)" "$(grep -o '[0-9\.]*' steam_buildid.txt)"
 
 echo "Done."
